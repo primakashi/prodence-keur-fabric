4e737c3b16263ef220497d13da0731dae3f7a1e4
primakashi <cc.primadani@gmail.com>
2019-10-23 20:15:14 +0700

    update script

--- setup.sh
+++ setup.sh
@@ -2,6 +2,5 @@ cd basic-network
 ./start.sh
 
 cd ../organization/prodence/configuration/cli/
-# ./monitordocker.sh
 
-docker-compose -f docker-compose.yml up -d
+docker-compose -f docker-compose.yml up -d prodence
