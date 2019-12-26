4836f887bb6e8f55e8057399cd979eda5696257c
primakashi <cc.primadani@gmail.com>
2019-12-26 21:26:50 +0700

    fix conflict

---
+++ backend/controllers/engine_transmission.js
@@ -0,0 +1,170 @@
+const CryptoJS = require('crypto-js');
+const newEngine_transmission = require('../../organization/prodence/mesin_transmisi/issue');
+const getOneEngine_transmission = require('../../organization/prodence/mesin_transmisi/getmesin_transmisi');
+const updateDataEngine_transmission = require('../../organization/prodence/mesin_transmisi/update');
+const deleteDataEngine_transmission = require('../../organization/prodence/mesin_transmisi/delete');
+const checkToken = require('./check_token');
+
+exports.getEngine_transmission = (req, res) => { // eslint-disable-line
+    try {
+        /**
+         * Check validation token data
+         * func in checkToken
+         */
+        const valid = checkToken.validateToken(req.headers.authorization);
+        // const valid = true;
+
+        if (valid) {
+            getOneEngine_transmission.getMesin_transmisi(req.body, (err,data)=>{
+                if (err) {
+                    res.status(400);
+                    res.send({
+                        err: true,
+                        message: 'Tidak dapat memproses data, '
+                            + 'harap hubungi administrator!',
+                    });
+                } else if (data) {
+                    res.status(200);
+                    res.send(data);
+                }
+            })
+        } else {
+            res.status(400);
+            res.send({
+                valid: false,
+                token: 'expired',
+                redirect: '/login',
+            });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500);
+        res.send({
+            err: true,
+            errors: 'Error bad request data',
+        });
+    }
+};
+
+exports.createEngine_transmission = (req, res) => { // eslint-disable-line
+    try {
+        /**
+         * Check validation token data
+         * func in checkToken
+         */
+        const valid = checkToken.validateToken(req.headers.authorization);
+        // const valid = true;
+
+        if (valid) {
+            newEngine_transmission.createMesin_transmisi(req.body, (err,data)=>{
+                if (err) {
+                    res.status(400);
+                    res.send({
+                        err: true,
+                        message: 'Tidak dapat memproses data, '
+                            + 'harap hubungi administrator!',
+                    });
+                } else if (data) {
+                    res.status(200);
+                    res.send(data);
+                }
+            })
+        } else {
+            res.status(400);
+            res.send({
+                valid: false,
+                token: 'expired',
+                redirect: '/login',
+            });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500);
+        res.send({
+            err: true,
+            errors: 'Error bad request data',
+        });
+    }
+};
+
+exports.updateEngine_transmission = (req, res) => { // eslint-disable-line
+    try {
+        /**
+         * Check validation token data
+         * func in checkToken
+         */
+        const valid = checkToken.validateToken(req.headers.authorization);
+        // const valid = true;
+
+        if (valid) {
+            updateDataEngine_transmission.updateMesin_transmisi(req.body, (err,data)=>{
+                if (err) {
+                    res.status(400);
+                    res.send({
+                        err: true,
+                        message: 'Tidak dapat memproses data, '
+                            + 'harap hubungi administrator!',
+                    });
+                } else if (data) {
+                    res.status(200);
+                    res.send(data);
+                }
+            })
+        } else {
+            res.status(400);
+            res.send({
+                valid: false,
+                token: 'expired',
+                redirect: '/login',
+            });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500);
+        res.send({
+            err: true,
+            errors: 'Error bad request data',
+        });
+    }
+};
+
+exports.deleteEngine_transmission = (req, res) => { // eslint-disable-line
+    try {
+        /**
+         * Check validation token data
+         * func in checkToken
+         */
+        const valid = checkToken.validateToken(req.headers.authorization);
+        // const valid = true;
+
+        if (valid) {
+            deleteDataEngine_transmission.deleteMesin_transmisi(req.body, (err,data)=>{
+                if (err) {
+                    res.status(400);
+                    res.send({
+                        err: true,
+                        message: 'Tidak dapat memproses data, '
+                            + 'harap hubungi administrator!',
+                    });
+                } else if (data) {
+                    res.status(200);
+                    res.send(data);
+                }
+            })
+        } else {
+            res.status(400);
+            res.send({
+                valid: false,
+                token: 'expired',
+                redirect: '/login',
+            });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500);
+        res.send({
+            err: true,
+            errors: 'Error bad request data',
+        });
+    }
+};
---
+++ organization/prodence/contract/mesin_transmisi/lib/mesin_transmisi.js
@@ -0,0 +1,82 @@
+'use strict';
+
+const State = require('./../ledger-api/state.js');
+
+const pState = {
+    CREATED: 1,
+    UPDATED: 2,
+    DELETED: 3
+};
+
+class Mesin_transmisi extends State {
+    constructor(obj) {
+        super(Mesin_transmisi.getClass(), [obj.no_pemeriksaan, obj.no_kendaraan]);
+        Object.assign(this, obj);
+    }
+
+    getKendaraa() {
+        return this.obj;
+    }
+
+    getNoKendaraan() {
+        return this.no_kendaraan;
+    }
+
+    setPemeriksaanMesin_transmisi(newDudukanMesin, newKondisiMesin, newTransmisi, newSistemGasBuang, newEmisiAsap, newEmisiCo, newStatus) {
+        this.dudukan_mesin = newDudukanMesin;
+        this.kondisi_mesin = newKondisiMesin;
+        this.transmisi = newTransmisi;
+        this.sistem_gas_buang = newSistemGasBuang;
+        this.emisi_asap= newEmisiAsap;
+        this.emisi_co = newEmisiCo;
+        this.status = newStatus;
+
+    }
+
+    setCreated() {
+        this.currentState = pState.CREATED;
+    }
+
+    setUpdated() {
+        this.currentState = pState.UPDATED;
+    }
+
+    setDeleted() {
+        this.currentState = pState.DELETED;
+    }
+
+    isCreated() {
+        return this.currentState === pState.CREATED;
+    }
+
+    isUpdated() {
+        return this.currentState === pState.UPDATED;
+    }
+
+    isDeleted() {
+        return this.currentState === pState.DELETED;
+    }
+
+    static fromBuffer(buffer) {
+        return Mesin_transmisi.deserialize(buffer);
+    }
+
+    toBuffer() {
+        return Buffer.from(JSON.stringify(this));
+    }
+
+    static deserialize(data) {
+        return State.deserializeClass(data, Mesin_transmisi);
+    }
+
+    static createInstance(no_pemeriksaan, no_kendaraan, dudukan_mesin, kondisi_mesin, transmisi, sistem_buang_gas, emisi_asap, emisi_co, status) {
+        return new Mesin_transmisi({ no_pemeriksaan, no_kendaraan, dudukan_mesin, kondisi_mesin, transmisi, sistem_buang_gas, emisi_asap, emisi_co, status});
+    }
+
+    static getClass() {
+        return 'org.prodence.mesin_transmisi';
+    }
+
+}
+
+module.exports = Mesin_transmisi;
