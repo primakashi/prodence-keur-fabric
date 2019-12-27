b364e7c08cb7ec1bf6a4c2b78079f41b1641304b
andredananjaya <andredananjaya1@gmail.com>
2019-12-27 11:55:59 +0700

    add contract brake

---
+++ backend/controllers/brake.js
@@ -0,0 +1,170 @@
+const CryptoJS = require('crypto-js');
+const newBrake = require('../../organization/prodence/sistem_rem/issue');
+const getOneBrake = require('../../organization/prodence/sistem_rem/getsistem_rem');
+const updateDataBrake = require('../../organization/prodence/sistem_rem/update');
+const deleteDataBrake = require('../../organization/prodence/sistem_rem/delete');
+const checkToken = require('./check_token');
+
+exports.getBrake = (req, res) => { // eslint-disable-line
+    try {
+        /**
+         * Check validation token data
+         * func in checkToken
+         */
+        const valid = checkToken.validateToken(req.headers.authorization);
+        // const valid = true;
+
+        if (valid) {
+            getOneBrake.getsistem_rem(req.body, (err,data)=>{
+                if (err) {
+                    res.status(400);
+                    res.send({
+                        err: true,
+                        message: 'Tidak dapat memproses data, '
+                            + 'harap hubungi administrator!',
+                    });
+                } else if (data) {
+                    res.status(200);
+                    res.send(data);
+                }
+            })
+        } else {
+            res.status(400);
+            res.send({
+                valid: false,
+                token: 'expired',
+                redirect: '/login',
+            });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500);
+        res.send({
+            err: true,
+            errors: 'Error bad request data',
+        });
+    }
+};
+
+exports.createBrake = (req, res) => { // eslint-disable-line
+    try {
+        /**
+         * Check validation token data
+         * func in checkToken
+         */
+        const valid = checkToken.validateToken(req.headers.authorization);
+        // const valid = true;
+
+        if (valid) {
+            newBrake.createSistem_rem(req.body, (err,data)=>{
+                if (err) {
+                    res.status(400);
+                    res.send({
+                        err: true,
+                        message: 'Tidak dapat memproses data, '
+                            + 'harap hubungi administrator!',
+                    });
+                } else if (data) {
+                    res.status(200);
+                    res.send(data);
+                }
+            })
+        } else {
+            res.status(400);
+            res.send({
+                valid: false,
+                token: 'expired',
+                redirect: '/login',
+            });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500);
+        res.send({
+            err: true,
+            errors: 'Error bad request data',
+        });
+    }
+};
+
+exports.updateBrake = (req, res) => { // eslint-disable-line
+    try {
+        /**
+         * Check validation token data
+         * func in checkToken
+         */
+        const valid = checkToken.validateToken(req.headers.authorization);
+        // const valid = true;
+
+        if (valid) {
+            updateDataBrake.updateSistem_rem(req.body, (err,data)=>{
+                if (err) {
+                    res.status(400);
+                    res.send({
+                        err: true,
+                        message: 'Tidak dapat memproses data, '
+                            + 'harap hubungi administrator!',
+                    });
+                } else if (data) {
+                    res.status(200);
+                    res.send(data);
+                }
+            })
+        } else {
+            res.status(400);
+            res.send({
+                valid: false,
+                token: 'expired',
+                redirect: '/login',
+            });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500);
+        res.send({
+            err: true,
+            errors: 'Error bad request data',
+        });
+    }
+};
+
+exports.deleteBrake = (req, res) => { // eslint-disable-line
+    try {
+        /**
+         * Check validation token data
+         * func in checkToken
+         */
+        const valid = checkToken.validateToken(req.headers.authorization);
+        // const valid = true;
+
+        if (valid) {
+            deleteDataBrake.deleteSistem_rem(req.body, (err,data)=>{
+                if (err) {
+                    res.status(400);
+                    res.send({
+                        err: true,
+                        message: 'Tidak dapat memproses data, '
+                            + 'harap hubungi administrator!',
+                    });
+                } else if (data) {
+                    res.status(200);
+                    res.send(data);
+                }
+            })
+        } else {
+            res.status(400);
+            res.send({
+                valid: false,
+                token: 'expired',
+                redirect: '/login',
+            });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500);
+        res.send({
+            err: true,
+            errors: 'Error bad request data',
+        });
+    }
+};
--- installcontract.sh
+++ installcontract.sh
@@ -19,5 +19,8 @@
 # docker exec prodence peer chaincode install -n bodyframecontract -v 0 -p /opt/gopath/src/github.com/contract/bodyframe -l node
 # docker exec prodence peer chaincode instantiate -n bodyframecontract -v 0 -l node -c '{"Args":["org.prodence.bodyframe:instantiate"]}' -C mychannel -P "AND ('Org1MSP.member')"
 
-docker exec prodence peer chaincode install -n suspensioncontract -v 0 -p /opt/gopath/src/github.com/contract/suspension -l node
-docker exec prodence peer chaincode instantiate -n suspensioncontract -v 0 -l node -c '{"Args":["org.prodence.suspension:instantiate"]}' -C mychannel -P "AND ('Org1MSP.member')"
+# docker exec prodence peer chaincode install -n suspensioncontract -v 0 -p /opt/gopath/src/github.com/contract/suspension -l node
+# docker exec prodence peer chaincode instantiate -n suspensioncontract -v 0 -l node -c '{"Args":["org.prodence.suspension:instantiate"]}' -C mychannel -P "AND ('Org1MSP.member')"
+
+docker exec prodence peer chaincode install -n sistem_remcontract -v 0 -p /opt/gopath/src/github.com/contract/sistem_rem -l node
+docker exec prodence peer chaincode instantiate -n sistem_remcontract -v 0 -l node -c '{"Args":["org.prodence.sistem_rem:instantiate"]}' -C mychannel -P "AND ('Org1MSP.member')"
